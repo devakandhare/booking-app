3b7606b773361e44dc4e76b18dcb4cd6c25b1061
Devram <kandharedeva87@gmail.com>
2022-01-02 22:47:12 +0530

    added functionality for showing user details

--- src/users/UsersList.js
+++ src/users/UsersList.js
@@ -1,24 +1,38 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import data from "../static.json";
 
 const { users } = data;
 
 function UsersList(props) {
   const [userIndex, setUserIndex] = useState(0);
+  const user = users[userIndex];
 
   const changeUserIndex = (selectedIndex) => {
     setUserIndex(selectedIndex);
   };
   return (
-    <ul className="users items-list-nav">
-      {users.map((u, i) => (
-        <li key={u.id} className={i === userIndex ? "selected" : null}>
-          <button className="btn" onClick={() => changeUserIndex(i)}>
-            {u.name}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <ul className="user-details items-list-nav">
+        {users.map((u, i) => (
+          <li key={u.id} className={i === userIndex ? "selected" : null}>
+            <button className="btn" onClick={() => changeUserIndex(i)}>
+              {u.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+      {/** show the details of user. */}
+      {user && (
+        <div className="user-details">
+          <div className="item">
+            <div className="item-header">
+              <h2>{user.name}</h2>
+            </div>
+            <p>{user.notes}</p>
+          </div>
+        </div>
+      )}
+    </Fragment>
   );
 }
 
