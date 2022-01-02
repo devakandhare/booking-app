1dcd6bfdcc067b1e77b803652843326272bfc045
Devram <kandharedeva87@gmail.com>
2022-01-02 22:13:39 +0530

    added functionality for adding state to the groups

--- src/bookables/BookableList.js
+++ src/bookables/BookableList.js
@@ -1,27 +1,56 @@
 import React, { useState } from "react";
 import data from "../static.json";
+import { FaArrowRight } from "react-icons/fa";
 
 const { bookables } = data;
 
 function BookableList(props) {
-  const group = "Rooms";
+  const [group, setGroup] = useState("Kit");
   const bookablesInGroup = bookables.filter((b) => b.group === group);
-  const [bookableIndex, setBookableIndex] = useState(1);
+  const [bookableIndex, setBookableIndex] = useState(0);
+
+  // get the set of groups
+  const groups = [...new Set(bookables.map((b) => b.group))];
 
   const changeBookableIndex = (selectedIndex) => {
     setBookableIndex(selectedIndex);
   };
 
+  const nextBookable = () => {
+    setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
+  };
+
+  const changeGroup = (e) => {
+    setGroup(e.target.value);
+    setBookableIndex(0);
+  };
+
   return (
-    <ul className="bookables items-list-nav">
-      {bookablesInGroup.map((b, i) => (
-        <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-          <button className="btn" onClick={() => changeBookableIndex(i)}>
-            {b.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      {/** add options to select the groups from the list */}
+      <select value={group} onChange={changeGroup}>
+        {groups.map((g) => (
+          <option key={g} value={g}>
+            {g}
+          </option>
+        ))}
+      </select>
+      <ul className="bookables items-list-nav">
+        {bookablesInGroup.map((b, i) => (
+          <li key={b.id} className={i === bookableIndex ? "selected" : null}>
+            <button className="btn" onClick={() => changeBookableIndex(i)}>
+              {b.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <p>
+        <button className="btn" onClick={nextBookable} autoFocus>
+          <FaArrowRight />
+          <span>Next</span>
+        </button>
+      </p>
+    </div>
   );
 }
 
